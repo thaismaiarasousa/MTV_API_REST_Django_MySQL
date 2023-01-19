179b5851cb0f91a420427e1d9e30ee34853d010c
thaismaiarasousa <thais-mns@hotmail.com>
2023-01-19 00:58:46 -0300

    dados sensíveis bd

--- Proyecto_API/settings.py
+++ Proyecto_API/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': '9Eminhasous@3',
+        'PASSWORD': ' ',
         'NAME': 'django_api',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
